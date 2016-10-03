cd7955456bb766accc990b1651dbe0d8a520aba6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-03 19:13:59 +0100

    Generate string of connected nodes (with configurable inter-node distance) from player to hook point and ensure each is connected in line to form a rope

--- Assets/scripts/Hook.cs
+++ Assets/scripts/Hook.cs
@@ -4,12 +4,49 @@ using System.Collections;
 public class Hook : MonoBehaviour {
 
 	public Vector2 hookPoint;
+	public GameObject lastNode;
 
 	[SerializeField]
+	private GameObject nodePrefab;
+	[SerializeField]
+	private Player player;
+	[SerializeField]
 	private float hookSpeed = 10;
+	[SerializeField]
+	private float nodeDis = 2;
+	private bool isHookLanded = false;
+
+	private void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+		lastNode = transform.gameObject;
+	}
 
 	private void Update () {
 		transform.position = Vector2.MoveTowards (transform.position, hookPoint, hookSpeed * Time.deltaTime);
+
+		if (transform.position != new Vector3 (hookPoint.x, hookPoint.y, transform.position.z)) {
+			if (Vector2.Distance (player.transform.position, lastNode.transform.position) > nodeDis) {
+				CreateNode ();
+			}
+		} else if (!isHookLanded) {
+			isHookLanded = true;
+			lastNode.GetComponent<HingeJoint2D> ().connectedBody = player.GetComponent<Rigidbody2D> ();
+		}
+	}
+
+	private void CreateNode () {
+		Vector2 nodePos = player.transform.position - lastNode.transform.position;
+		nodePos.Normalize ();
+		nodePos *= nodeDis;
+		nodePos += (Vector2)lastNode.transform.position;
+
+		GameObject newNode = Instantiate (nodePrefab, nodePos, Quaternion.identity) as GameObject;
+
+		newNode.transform.SetParent (transform);
+
+		lastNode.GetComponent<HingeJoint2D> ().connectedBody = newNode.GetComponent<Rigidbody2D> ();
+
+		lastNode = newNode;
 	}
 
 }
