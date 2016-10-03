3359383fa6b9f596833f4bb97599d236c0f2fb7c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-10-03 18:51:32 +0100

    Add hook class and give it a function to move towards the mouse position on click

---
+++ Assets/scripts/Hook.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hook : MonoBehaviour {
+
+	public Vector2 hookPoint;
+
+	[SerializeField]
+	private float hookSpeed = 10;
+
+	private void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, hookPoint, hookSpeed * Time.deltaTime);
+	}
+
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private Hook hookPrefab;
+
+	private Hook currentHook;
+
+	private void Update () {
+		CreateHookAtPoint ();
+	}
+
+	private void CreateHookAtPoint () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 aimPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			currentHook = Instantiate (hookPrefab, transform.position, Quaternion.identity) as Hook;
+			currentHook.hookPoint = aimPoint;
+		}
+	}
+}
